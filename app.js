5fd1f9125d6a0597d14d9bc59834a4b36493b54a
Sofia Morais <sofiassmorais@gmail.com>
2024-06-19 20:57:56 -0300

    refactor: Modificando repositório e if

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 alert('Olá Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 let numeroEscolhido;
-let tentativas = 1;
+let contadorTentativas = 1;
 
 while (numeroEscolhido != numeroSecreto) {
     numeroEscolhido = prompt('Escolha um número entre 1 e 30');
@@ -15,10 +15,8 @@ while (numeroEscolhido != numeroSecreto) {
             alert(`O número secreto é maior que ${numeroEscolhido}`);
         }
     }
-    tentativas++;
+    contadorTentativas++;
 }
 
-if(tentativas > 1 ) 
-alert(`Você descobriu o número secreto: ${numeroSecreto} com ${tentativas} tentativas`);
-else
-alert(`Você descobriu o número secreto: ${numeroSecreto} com ${tentativas} tentativa`);
+let stringTentativa = contadorTentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Você descobriu o número secreto: ${numeroSecreto} com ${contadorTentativas} ${stringTentativa}`);
